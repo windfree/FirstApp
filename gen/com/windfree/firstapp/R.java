41109cc1bff17b0e70c6cb5e2e215cf41c39c8e0
windfree <zhliu.work@gmail.com>
2014-09-14 21:32:53 +0800

    taskPhotoTest
    can take photo successfully but get null data when return to my
    activity. an issue on some samsung devices
    http://blog.csdn.net/wsyjx22/article/details/17412007 still not work

--- gen/com/windfree/firstapp/R.java
+++ gen/com/windfree/firstapp/R.java
@@ -997,8 +997,8 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_search=0x7f050040;
-        public static final int action_settings=0x7f05003f;
+        public static final int action_search=0x7f050041;
+        public static final int action_settings=0x7f050040;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1026,6 +1026,7 @@ containing a value of this type.
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
+        public static final int photo_icon=0x7f05003f;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
@@ -1161,6 +1162,7 @@ containing a value of this type.
         public static final int edit_message=0x7f0a000e;
         public static final int hello_world=0x7f0a0014;
         public static final int saved_high_score=0x7f0a0016;
+        public static final int take_photo_btn_label=0x7f0a0017;
         public static final int title_activity_display_message=0x7f0a0013;
         public static final int title_activity_main=0x7f0a0012;
     }
--- src/com/windfree/firstapp/MainActivity.java
+++ src/com/windfree/firstapp/MainActivity.java
@@ -2,12 +2,20 @@ package com.windfree.firstapp;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
@@ -17,18 +25,30 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
 	public final static String EXTRA_MESSAGE = "com.windfree.myfirstapp.MESSAGE";
 	private final static String LOG_TAG = "FirstApp";
 
+	static final int REQUEST_IMAGE_CAPTURE = 1;
+	private String mCurrentPhotoPath;
+	private ImageView m_photo_icon;
 
+	@Override
+	protected void onResume() {
+	    // TODO Auto-generated method stub
+	    super.onResume();
+	}
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		
+		this.m_photo_icon = (ImageView)findViewById(R.id.photo_icon);
+		
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
@@ -36,6 +56,18 @@ public class MainActivity extends ActionBarActivity {
 		
 	}
 
+	@Override
+	protected void onDestroy() {
+		Log.e(LOG_TAG, "onDestroy()");
+		super.onDestroy();
+	}
+
+	@Override
+	protected void onPause() {
+		Log.e(LOG_TAG, "onPause()");
+		super.onPause();
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 //		 Inflate the menu items for use in the action bar
@@ -189,7 +221,7 @@ public class MainActivity extends ActionBarActivity {
 	        stream.close();  
 	          
 	    } catch(Exception e) {  
-	        Log.e("TestFile", "Error on writeFilToSD.");  
+	        Log.e(LOG_TAG, "Error on writeFilToSD.");  
 	        e.printStackTrace();  
 	    }
 //		try{
@@ -200,5 +232,111 @@ public class MainActivity extends ActionBarActivity {
 //			e.printStackTrace();
 //		}
 	}
+	
+	///////////////////////////////////////Take Photo start
+//	private void dispatchTakePictureIntent() {
+//	    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//	    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+//	        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+//	    }
+//	}
+	
+	public void onConfigurationChanged(Configuration newConfig) {  
+        // TODO Auto-generated method stub  
+        Log.i("UserInfoActivity", "onConfigurationChanged");  
+        if(newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {  
+            Log.i(LOG_TAG, "∫·∆¡");  
+            Configuration o = newConfig;  
+            o.orientation = Configuration.ORIENTATION_PORTRAIT;  
+            newConfig.setTo(o);  
+        } else if(newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {  
+            Log.i(LOG_TAG, " ˙∆¡");  
+        }  
+        super.onConfigurationChanged(newConfig);  
+    }
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+			if (data != null) {
+				Bundle extras = data.getExtras();
+				Bitmap imageBitmap = (Bitmap) extras.get("data");
+				m_photo_icon.setImageBitmap(imageBitmap);
+				galleryAddPic();
+				setPic();
+			} else {
+				Log.e(LOG_TAG, "onActiveResult data is null!");
+			}
+		}
+	}
+	
+	private File createImageFile() throws IOException {
+	    // Create an image file name
+	    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+	    String imageFileName = "JPEG_" + timeStamp + "_";
+	    File storageDir = Environment.getExternalStoragePublicDirectory(
+	            Environment.DIRECTORY_PICTURES);
+	    File image = File.createTempFile(
+	        imageFileName,  /* prefix */
+	        ".jpg",         /* suffix */
+	        storageDir      /* directory */
+	    );
 
+	    // Save a file: path for use with ACTION_VIEW intents
+	    mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+	    return image;
+	}
+	
+	public void takePhoto(View view) {
+		Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+	    // Ensure that there's a camera activity to handle the intent
+	    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+	        // Create the File where the photo should go
+	        File photoFile = null;
+	        try {
+	            photoFile = createImageFile();
+	        } catch (IOException ex) {
+	            // Error occurred while creating the File
+	        }
+	        // Continue only if the File was successfully created
+	        if (photoFile != null) {
+	            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+	                    Uri.fromFile(photoFile));
+	            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+	        }
+	    }
+	}
+	
+	private void galleryAddPic() {
+	    Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+	    File f = new File(mCurrentPhotoPath);
+	    Uri contentUri = Uri.fromFile(f);
+	    mediaScanIntent.setData(contentUri);
+	    this.sendBroadcast(mediaScanIntent);
+	}
+	
+	private void setPic() {
+	    // Get the dimensions of the View
+	    int targetW = m_photo_icon.getWidth();
+	    int targetH = m_photo_icon.getHeight();
+
+	    // Get the dimensions of the bitmap
+	    BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+	    bmOptions.inJustDecodeBounds = true;
+	    BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+	    int photoW = bmOptions.outWidth;
+	    int photoH = bmOptions.outHeight;
+
+	    // Determine how much to scale down the image
+	    int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+	    // Decode the image file into a Bitmap sized to fill the View
+	    bmOptions.inJustDecodeBounds = false;
+	    bmOptions.inSampleSize = scaleFactor;
+	    bmOptions.inPurgeable = true;
+
+	    Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+	    m_photo_icon.setImageBitmap(bitmap);
+	}
 }
